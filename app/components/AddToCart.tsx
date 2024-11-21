76d8dfce800d2f1db1e5b63c2ab0dc07b2c054fb
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2024-11-21 14:11:33 -0500

    add users page boilerplate

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("Clicked Add To Cart")}>
+        {" "}
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
