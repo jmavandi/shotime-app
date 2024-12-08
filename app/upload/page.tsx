96fe2e62f8bb595c935e796912e81c397e2fc1c9
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2024-12-07 23:36:39 -0500

    add upload with cloudinary

---
+++ app/upload/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
+  return (
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="shohei card" />
+      )}
+      <CldUploadWidget
+        uploadPreset="djxhiq9ff"
+        options={{
+          sources: ["local"],
+        }}
+        onSuccess={(results) => {
+          const info = results.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
