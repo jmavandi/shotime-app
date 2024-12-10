c103e451eaf00a70c5083f3584b7b95a5d632734
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2024-12-10 18:30:07 -0500

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { auth } from "@/auth";
+import shotimeLogo from "@/public/images/ShotimeLogo.jpg";
 
 export default async function Home() {
   const session = await auth();
 
   return (
     <main>
+      <Image src={shotimeLogo} alt="Shotime Logo" />
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users"> Users </Link>
       <ProductCard />
--- auth.config.ts
+++ auth.config.ts
@@ -6,7 +6,6 @@ import bcrypt from "bcryptjs";
 
 import type { NextAuthConfig } from "next-auth";
 
-// Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
     Google,
